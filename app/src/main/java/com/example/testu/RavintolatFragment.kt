5b0d9eca364a5c393b4b9d31d64a9beb3d779c49
Honkanen Tatu <tatu.honkanen@edu.lapinamk.fi>
2020-01-17 11:10:10 +0200

    Testinummar1

---
+++ app/src/main/java/com/example/testu/RavintolatFragment.kt
@@ -0,0 +1,15 @@
+package com.example.testu
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class RavintolatFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_ravintolat, container, false)
+    }
+
+}
